a1c0e647b9eb7962bae0686b7757104d649c3c95
Lucas Almeida Siqueira <lucasalmeida70@gmail.com>
2020-12-04 23:16:33 -0300

    List filters

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,6 +12,7 @@ const List = styled.ul`
 function Home() {
   const [addresses, setAddresses] = useState(null);
   const [addressesArray, setAddressesArray] = useState(null);
+  const [sortingType, setSortingType] = useState('default');
 
   const getAddresses = (addresses) => {
     const array = Object.values(addresses);
@@ -36,6 +37,30 @@ function Home() {
     getAddresses(newAddresses);
   }
 
+  const handleChangeSorting = e => {
+    const newSortingType = e.target.value;
+    console.log(newSortingType, sortingType);
+
+    if(sortingType !== newSortingType) {
+      if(newSortingType === 'default') {
+        const sortedById = addressesArray.sort((a, b) => (b.id - a.id));
+        const sortedByTypes = sortedById.sort((a, b) => (b.address_types.length - a.address_types.length));
+        setAddressesArray(sortedByTypes);
+      }
+
+      if(newSortingType === 'byName') {
+        console.log('entrou')
+        setAddressesArray([...addressesArray].sort((a, b) => (a.name > b.name ? 1 : -1)));
+      }
+
+      if(newSortingType === 'byAddress') {
+        setAddressesArray([...addressesArray].sort((a, b) => (a.address > b.address ? 1 : -1)));
+      }
+
+      setSortingType(newSortingType);
+    }
+  }
+
   return (
     <>
       <PageTitle>
@@ -43,6 +68,13 @@ function Home() {
       </PageTitle>
       <div style={{textAlign: 'right'}}>
         <Link to="/create-address">+ Create an Address</Link>
+      </div>
+      <div>
+        <select onChange={handleChangeSorting} value={sortingType}>
+          <option value="default">No sorting</option>
+          <option value="byName">Sort by name (ASC)</option>
+          <option value="byAddress">Sort by address (ASC)</option>
+        </select>
       </div>
       <List>
         {addressesArray === null
